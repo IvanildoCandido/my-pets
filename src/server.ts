d325ea7e5d5645c20c717d366a6e521d52b8bc58
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-07-18 17:39:43 -0300

    final project 1.0

--- src/server.ts
+++ src/server.ts
@@ -12,14 +12,14 @@ const server = express();
 
 server.set('view engine', 'mustache');
 server.set('views', path.join(__dirname, 'views'));
-server.set('engine', mustache());
+server.engine('mustache', mustache());
 
 server.use(express.static(path.join(__dirname, '../public')));
 
 server.use(mainRoutes);
 
 server.use((req, res) => {
-  res.send('Página não encontrada!');
+  res.render('pages/404');
 });
 
 server.listen(PORT, () => {
